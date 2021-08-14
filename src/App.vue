d46e246ec2d0ff796eb4e788af036221bfa1fc20
jay16asd08 <jay16asd08@gmail.com>
2021-08-14 23:11:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld/> -->
+    <!--<router-view name="menu"></router-view>-->
+    <div >
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    const api = `${process.env.APIPATH}/api/${process.env.COSTOMPATH}/products`;//'https://vue-course-api.hexschool.io/api/leviathan91608/products';
+    //API 伺服器路徑
+    // 所申請的APIPath
+    // console.log(process.env.APIPATH, process.env.COSTOMPATH)
+
+    this.$http.get(api).then((response) => {
+      console.log(response.data)
+    });
+  },
+};
+</script>
+<style lang="scss">
+  @import "./assets/all";
+</style>
+
